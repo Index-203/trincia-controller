76e5490247dfdc146d86ced32f799dd1374aea64
Index-203 <gusifranz@gmail.com>
2023-04-08 00:21:02 +0200

    first change

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,45 @@
 #include <Arduino.h>
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+#include <Arduino_FreeRTOS.h>
+
+//rf24
+RF24 radio(7, 8); //CE, CSN
+const byte address[6] = "00001";
+
+//struct for data transmitter
+struct Data_Pack {
+int throttle_value;
+int shifter_gear_value;
+bool shifter_clutch_value;
+int track_right_mode_value;
+int track_left_mode_value;
+bool mower_engage_value;
+bool safe_stop;
+int piston_height;
+int pisto_track_unlock;
+};
+
 
 void setup() {
-  // put your setup code here, to run once:
+  //rf24 connection initialization
+    radio.begin();
+    radio.setChannel(125);
+    radio.openReadingPipe(0, address);
+    radio.setPALevel(RF24_PA_MAX);
+    radio.startListening();
+
+  //struct initialize
+    struct Data_Pack initial_values{50, 0, LOW, 3, 3, LOW,LOW,0,0}; //pls remember Franz to insert correct value for servo control
+
+
+
+
+
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-}
+}          
